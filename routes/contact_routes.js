feb3f4c922d7218d3335ab6875b61819e72f5e32
Emmanuel <attahzuzu@gmail.com>
2024-03-16 15:50:45 +0100

    Made all contact route private(require authentication)

--- routes/contact_routes.js
+++ routes/contact_routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const {getContacts,createContact,updateContact,deleteContact} = require("../controllers/contact_controller.js")
+const {getContacts,createContact,updateContact,deleteContact} = require("../controllers/contact_controller.js");
+const validateToken = require("../middleware/auth_middleware.js");
 
 const router = express.Router()
 
+
+router.use(validateToken)
 router.route("/").get(getContacts).post(createContact)
-
 router.route("/:id").put(updateContact).delete(deleteContact)
-
 
 
 module.exports = router;
